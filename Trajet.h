4df1a16641d59dab313fb41f8f2299a569ae72da
Damien <48095764+DamienCarreau@users.noreply.github.com>
2020-01-09 12:30:45 +0100

    Update Trajet.h

--- Trajet.h
+++ Trajet.h
@@ -2,8 +2,8 @@
                     Classe Trajet  - Représente un trajet
                              -------------------
     début                : 22/11/2019
-    copyright            : (C) 2019 par J.HUE, F.HACHIM
-    e-mail               : jerome.hue@insa-lyon.fr / faouz.hachim@insa-lyon.fr
+    copyright            : (C) 2019 par J.HUE, D.CARREAU
+    e-mail               : jerome.hue@insa-lyon.fr / damien.carreau@insa-lyon.fr
 *************************************************************************/
 
 //---------- Interface de la classe <Trajet> (fichier Trajet.h) ----------------
